2789b8268f361f37e854ee0a801f829013480944
Eidanv <38049259+Eidanv@users.noreply.github.com>
2018-12-16 22:34:17 +0600

    Add files via upload

---
+++ ConsoleApp5/Short.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+
+
+namespace OracleGetData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conString = "User Id = SYSTEM; password = Guitar1990; Data Source=localhost:1521/Elastic";
+
+            OracleConnection con = new OracleConnection();
+            con.ConnectionString = conString;
+            con.Open();
+
+            OracleCommand cmd = con.CreateCommand();
+            cmd.CommandText = "SELECT CITY FROM countries WHERE ID = 800";
+
+            OracleDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine("gsdgssdg: " + reader.GetString(0));
+            }
+            Console.ReadLine();
+        }
+    }
+}
